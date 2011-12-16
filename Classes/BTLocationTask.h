2d9a082cc3e63af5883a8c2935417c89ca42ac32
Charlie Groves <charlie.groves@gmail.com>
2011-12-15 18:15:48 -0800

    Not used

--- Classes/BTLocationTask.h
+++
@@ -1,8 +0,0 @@
-//
-//  Betwixt - Copyright 2011 Three Rings Design
-
-#import "BTObject.h"
-
-@interface BTLocationTask : BTObject
-
-@end
--- Classes/BTLocationTask.m
+++
@@ -1,8 +0,0 @@
-//
-//  Betwixt - Copyright 2011 Three Rings Design
-
-#import "BTLocationTask.h"
-
-@implementation BTLocationTask
-
-@end
