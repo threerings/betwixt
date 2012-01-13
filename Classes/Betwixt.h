2745a4eafde8acc6486d2147e84baa9211028b04
Tim Conkling <tconkling@gmail.com>
2012-01-13 14:54:31 -0800

    Betwixt.h master include file

---
+++ Classes/Betwixt.h
@@ -0,0 +1,38 @@
+//
+//  Betwixt - Copyright 2011 Three Rings Design
+
+// Dependencies
+#import "React.h"
+#import "Sparrow.h"
+
+#import "BTApplicationDelegate.h"
+#import "BTContext.h"
+#import "BTDisplayable.h"
+#import "BTDisplayContext.h"
+#import "BTInterpolator.h"
+#import "BTKeyed.h"
+#import "BTMode.h"
+#import "BTModeStack.h"
+#import "BTNode.h"
+#import "BTObject.h"
+#import "BTSprite.h"
+#import "SPEventDispatcher+BlockListener.h"
+
+// Resources
+#import "GDataXMLException.h"
+#import "GDataXMLNode+OOO.h"
+#import "BTLoadable.h"
+#import "BTLoadableBatch.h"
+#import "BTResource.h"
+#import "BTResourceFactory.h"
+#import "BTResourceGroup.h"
+#import "BTResourceManager.h"
+
+// Tasks
+#import "BTAlphaTask.h"
+#import "BTBlockTask.h"
+#import "BTDelayTask.h"
+#import "BTDetachTask.h"
+#import "BTLocationTask.h"
+#import "BTRotationTask.h"
+#import "BTTaskSequence.h"
