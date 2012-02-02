9c941832c493b55b97089759d7cee41670c97609
Tim Conkling <tconkling@gmail.com>
2012-02-02 13:44:11 -0800

    add the "require nil termination" sentinel to these varargs method declarations

--- Classes/BTParallelTask.h
+++ Classes/BTParallelTask.h
@@ -1,9 +1,8 @@
 //
 // Betwixt - Copyright 2012 Three Rings Design
 
-#import <Foundation/Foundation.h>
 #import "BTNode.h"
 
 @interface BTParallelTask : BTNode
-+ (BTParallelTask*)withNodes:(BTNode*)node, ...;
++ (BTParallelTask*)withNodes:(BTNode*)node, ... NS_REQUIRES_NIL_TERMINATION;
 @end
--- Classes/BTSequenceTask.h
+++ Classes/BTSequenceTask.h
@@ -1,9 +1,8 @@
 //
 // Betwixt - Copyright 2012 Three Rings Design
 
-#import <Foundation/Foundation.h>
 #import "BTNode.h"
 
 @interface BTSequenceTask : BTNode
-+ (BTSequenceTask*)withNodes:(BTNode*)node, ...;
++ (BTSequenceTask*)withNodes:(BTNode*)node, ... NS_REQUIRES_NIL_TERMINATION;
 @end
