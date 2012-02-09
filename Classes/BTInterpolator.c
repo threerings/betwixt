81f1a6faf5568e6bc5bf103bc8746164f3543474
Tim Conkling <tconkling@gmail.com>
2012-02-08 18:25:21 -0800

    BTInterpolate, a convenience function for using interpolators

--- Classes/BTInterpolator.c
+++ Classes/BTInterpolator.c
@@ -3,6 +3,8 @@
 
 #include "BTInterpolator.h"
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
 BTInterpolator BTLinearInterpolator = ^ float (float ratio) { return ratio; };
 BTInterpolator BTEaseInInterpolator = ^ float (float ratio) { return ratio * ratio * ratio; };
 BTInterpolator BTEaseOutInterpolator = ^ float (float ratio) {
@@ -13,3 +15,9 @@ BTInterpolator BTEaseInOutInterpolator = ^ float (float ratio) {
     if (ratio < 0.5f) return 0.5f * BTEaseOutInterpolator(ratio * 2.0f);
     else  return 0.5f * BTEaseInInterpolator((ratio - 0.5f) * 2.0f) + 0.5f;
 };
+
+float BTInterpolate (float from, float to, float elapsedTime, float totalTime, 
+                     BTInterpolator interp) {
+    elapsedTime = MIN(elapsedTime, totalTime);
+    return from + ((to - from) * interp(elapsedTime / totalTime));
+}
--- Classes/BTInterpolator.h
+++ Classes/BTInterpolator.h
@@ -7,3 +7,6 @@ BTInterpolator BTLinearInterpolator;
 BTInterpolator BTEaseInInterpolator;
 BTInterpolator BTEaseOutInterpolator;
 BTInterpolator BTEaseInOutInterpolator;
+
+float BTInterpolate (float from, float to, float elapsedTime, float totalTime, 
+                     BTInterpolator interp);
