84b10e78255d669e4f9b76e06dfd42c865949f2e
Tim Conkling <tconkling@gmail.com>
2012-02-11 14:18:54 -0800

    CLAMP macro

--- Classes/BTMath.h
+++ Classes/BTMath.h
@@ -1,11 +1,13 @@
 //
 // Betwixt - Copyright 2012 Three Rings Design
 
+#define CLAMP(val,min,max) ((val)<(min)?(min):((val)>(max)?(max):(val)))
+
 @interface BTMath : NSObject
 
-+ (float) distanceFromX:(float)fromX fromY:(float)fromY toX:(float)toX toY:(float)toY;
-+ (float) distanceFrom:(SPPoint*)p1 to:(SPPoint*)p2;
-+ (float) distanceSqFromX:(float)fromX fromY:(float)fromY toX:(float)toX toY:(float)toY;
-+ (float) distanceSqFrom:(SPPoint*)p1 to:(SPPoint*)p2;
++ (float)distanceFromX:(float)fromX fromY:(float)fromY toX:(float)toX toY:(float)toY;
++ (float)distanceFrom:(SPPoint*)p1 to:(SPPoint*)p2;
++ (float)distanceSqFromX:(float)fromX fromY:(float)fromY toX:(float)toX toY:(float)toY;
++ (float)distanceSqFrom:(SPPoint*)p1 to:(SPPoint*)p2;
 
 @end
