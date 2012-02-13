c688099b2411148a2439161737c05e2848839a43
Tim Conkling <tconkling@gmail.com>
2012-02-13 13:21:14 -0800

    BTUtils.BTCompareInts, BTCompareFloats

--- Classes/BTUtils.h
+++ Classes/BTUtils.h
@@ -3,3 +3,7 @@
 
 id BTNilToNSNull(id obj);
 id BTNSNullToNil(id obj);
+
+/// return: (a<b):-1 (a>b):1 (a==b):0
+int BTCompareInts (int a, int b);
+int BTCompareFloats (float a, float b);
--- Classes/BTUtils.m
+++ Classes/BTUtils.m
@@ -6,3 +6,9 @@
 id BTNSNullToNil (id obj) { return (obj == [NSNull null] ? nil : obj); }
 
 id BTNilToNSNull (id obj) { return (obj == nil ? [NSNull null] : obj); }
+
+#define COMPARE(a,b) ((a < b ? -1 : (a > b ? 1 : 0)))
+
+int BTCompareInts (int a, int b) { return COMPARE(a,b); }
+
+int BTCompareFloats (float a, float b) { return COMPARE(a,b); }
