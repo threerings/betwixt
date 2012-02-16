de5eb70bae5578eb39044370be5ed2e63f4df13b
Charlie Groves <charlie.groves@gmail.com>
2012-02-16 10:02:10 -0800

    The flipbook snapshot layer is now named flipbook

--- Classes/BTMovieResource.m
+++ Classes/BTMovieResource.m
@@ -26,7 +26,7 @@
     GDataXMLElement* layersEl = [xml requireChild:@"DOMSymbolItem/timeline/DOMTimeline/layers"];
     int frames = 0;
     NSArray* layerEls = [layersEl elements];
-    if ([[[layerEls objectAtIndex:0] stringAttribute:@"name"] isEqualToString:@"snapshots"]) {
+    if ([[[layerEls objectAtIndex:0] stringAttribute:@"name"] isEqualToString:@"flipbook"]) {
         BTMovieResourceLayer* layer = [[BTMovieResourceLayer alloc] initFlipbookNamed:[xml stringAttribute:@"name"] withXml:[layerEls objectAtIndex:0]];
         [layers addObject:layer];
         frames = layer.frames;
