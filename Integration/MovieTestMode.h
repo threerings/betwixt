c1dc6b04d32823b4d6eb4b87ecead6b455ad2991
Tim Conkling <tconkling@gmail.com>
2012-03-05 19:18:53 -0800

    MovieTestMode utiilty

---
+++ Integration/MovieTestMode.h
@@ -0,0 +1,10 @@
+//
+// Betwixt - Copyright 2012 Three Rings Design
+
+#import "BTMode.h"
+
+@interface MovieTestMode : BTMode
+
+- (id)initWithResourceName:(NSString*)resourceName movieName:(NSString*)movieName;
+
+@end
---
+++ Integration/MovieTestMode.m
@@ -0,0 +1,43 @@
+//
+// Betwixt - Copyright 2012 Three Rings Design
+
+#import "MovieTestMode.h"
+#import "BTMode+Protected.h"
+#import "BTApp.h"
+#import "BTResourceManager.h"
+#import "BTMovie.h"
+#import "BTMovieResource.h"
+
+@implementation MovieTestMode {
+@protected
+    NSString* _resourceName;
+    NSString* _movieName;
+}
+
+- (id)initWithResourceName:(NSString *)resourceName movieName:(NSString *)movieName {
+    if (!(self = [super init])) {
+        return nil;
+    }
+    _resourceName = resourceName;
+    _movieName = movieName;
+    return self;
+}
+
+- (void)setup {
+    [super setup];
+    [BTApp.app.resourceManager loadResourceFile:_resourceName];
+    
+    BTMovie* movie = [BTMovieResource newMovie:_movieName];
+    SPRectangle* bounds = [movie bounds];
+    movie.x = ((BTApp.app.viewSize.x - bounds.width) * 0.5f) - bounds.x;
+    movie.y = ((BTApp.app.viewSize.y - bounds.height) * 0.5f) - bounds.y;
+    
+    [self.sprite addChild:movie];
+}
+
+- (void)destroy {
+    [BTApp.app.resourceManager unloadResourceFile:_resourceName];
+    [super destroy];
+}
+
+@end
