f8cda674be3ce0db096fe82af4a5034f487ac9f5
Tim Conkling <tconkling@gmail.com>
2012-03-13 13:31:50 -0700

    Remove unnecessary Foundation imports

--- Classes/BTDetachTask.h
+++ Classes/BTDetachTask.h
@@ -1,7 +1,6 @@
 //
 // Betwixt - Copyright 2012 Three Rings Design
 
-#import <Foundation/Foundation.h>
 #import "BTNode.h"
 
 @interface BTDetachTask : BTNode
--- Classes/BTMovie+Package.h
+++ Classes/BTMovie+Package.h
@@ -1,7 +1,6 @@
 //
 // Betwixt - Copyright 2012 Three Rings Design
 
-#import <Foundation/Foundation.h>
 
 @interface BTMovie (package)
 - (id)initWithLayers:(NSMutableArray*)layers andLabels:(NSArray*)labels;
