f9cd5ad412f73247cd3493ffca60ba63aed3f9aa
Tim Conkling <tconkling@gmail.com>
2012-04-10 16:02:13 -0700

    NSArray.binarySearch

--- Classes/NSArray+Extensions.m
+++ Classes/NSArray+Extensions.m
@@ -6,7 +6,7 @@
 
 @implementation NSArray (OOOExtensions)
 
-- (NSMutableArray *)filter:(BOOL (^)(id))block {
+- (NSMutableArray*)filter:(BOOL (^)(id))block {
     NSMutableArray *array = [[NSMutableArray alloc] init];
     for (id obj in self) {
         if (block(obj)) {
@@ -16,7 +16,7 @@
     return array;
 }
 
-- (NSMutableArray *)map:(id (^)(id))block {
+- (NSMutableArray*)map:(id (^)(id))block {
     NSMutableArray *array = [[NSMutableArray alloc] initWithCapacity:self.count];
     for (id obj in self) {
         [array addObject:BTNilToNSNull(block(obj))];
@@ -31,6 +31,28 @@
         }
     }
     return nil;
+}
+
+- (int)binarySearch:(id)object offset:(int)offset length:(int)length comp:(NSComparator)comp {
+    int low = offset;
+    int high = offset + length - 1;
+    while (low <= high) {
+        int mid = ((unsigned int)low + (unsigned int)high) >> 1;
+        id midVal = [self objectAtIndex:mid];
+        int cmp = comp(midVal, object);
+        if (cmp < 0) {
+            low = mid + 1;
+        } else if (cmp > 0) {
+            high = mid - 1;
+        } else {
+            return mid; // key found
+        }
+    }
+    return -(low + 1); // key not found.
+}
+
+- (int)binarySearch:(id)object comp:(NSComparator)comp {
+    return [self binarySearch:object offset:0 length:self.count comp:comp];
 }
 
 @end
