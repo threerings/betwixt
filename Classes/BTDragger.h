4bc51e6c2f8b0f59b6ba52bd2b84c3c27e126eeb
Tim Conkling <tconkling@gmail.com>
2012-04-13 16:00:01 -0700

    simplify BTDragger
    Most implementations will want to create a BTDragger in response to a touch event,
    instead of having the dragger object around all the time.

--- Classes/BTDragger.h
+++ Classes/BTDragger.h
@@ -6,34 +6,18 @@
 
 @class BTDisplayObject;
 
-typedef void (^BTDragStartBlock)(SPPoint* start);
-typedef void (^BTDraggedBlock)(SPPoint* current, SPPoint* start);
-
-/// Listens for touches on the given target.
-/// When the target is touched, a drag will start.
-/// While a drag is in progress, no other displayObjects will receive touch events.
-
 @interface BTDragger : BTObject <BTTouchListener> {
 @protected
-    __weak BTDisplayObject* _target;
     SPPoint* _start;
     SPPoint* _current;
-    RAConnection* _touchBeganConn;
     BTInputRegistration* _dragReg;
 }
 
-@property (nonatomic,assign) BOOL enabled; // default: YES
 @property (nonatomic,readonly) BOOL dragging;
 
-+ (BTDragger*)withTarget:(BTDisplayObject*)target 
-             onDragStart:(BTDragStartBlock)onDragStart
-               onDragged:(BTDraggedBlock)onDragged
-               onDragEnd:(BTDraggedBlock)onDragEnd;
-
-/// Designated initializer
-- (id)initWithTarget:(BTDisplayObject*)target enabled:(BOOL)enabled;
-- (id)initWithTarget:(BTDisplayObject*)target;
-
+/// While a drag is in progress, no other displayObjects will receive touch events.
+- (void)startDragWithTouch:(SPTouch*)touch;
+- (void)startDragWithScreenLoc:(SPPoint*)globalLoc;
 - (void)cancelDrag;
 
 /// subclasses should override these to do something
--- Classes/BTDragger.m
+++ Classes/BTDragger.m
@@ -6,69 +6,27 @@
 #import "BTDisplayObject.h"
 #import "BTMode.h"
 
-@interface BTCallbackDragger : BTDragger
-@property(nonatomic,copy) BTDragStartBlock onDragStart;
-@property(nonatomic,copy) BTDraggedBlock onDragged;
-@property(nonatomic,copy) BTDraggedBlock onDragEnd;
-@end
-
 @implementation BTDragger {
     BTInputRegistration* _draggerReg;
 }
 
-+ (BTDragger*)withTarget:(BTDisplayObject*)target 
-             onDragStart:(BTDragStartBlock)onDragStart 
-               onDragged:(BTDraggedBlock)onDragged 
-               onDragEnd:(BTDraggedBlock)onDragEnd {
-    BTCallbackDragger* dragger = [[BTCallbackDragger alloc] init];
-    dragger.onDragStart = onDragStart;
-    dragger.onDragged = onDragged;
-    dragger.onDragEnd = onDragEnd;
-    return dragger;
-}
-
-- (id)initWithTarget:(BTDisplayObject*)target enabled:(BOOL)enabled {
-    if (!(self = [super init])) {
-        return nil;
-    }
-    _target = target;
-    self.enabled = enabled;
-    return self;
-}
-
-- (id)initWithTarget:(BTDisplayObject*)target {
-    return [self initWithTarget:target enabled:YES];
-}
-
 - (void)cleanup {
+    [self cancelDrag];
     [super cleanup];
-}
-
-- (BOOL)enabled {
-    return _touchBeganConn != nil;
 }
 
 - (BOOL)dragging {
     return _draggerReg != nil;
 }
 
-- (void)setEnabled:(BOOL)enabled {
-    if (self.enabled == enabled) {
-        return;
-    }
-    
-    if (enabled) {
-        _touchBeganConn = [self.conns onObjectReactor:_target.touchBegan connectSlot:^(SPTouch* touch) {
-            _draggerReg = [self.mode.input registerListener:self];
-            _start = [SPPoint pointWithX:touch.globalX y:touch.globalY];
-            [self onDragStart:_start];
-        }];
-        
-    } else {
-        [_touchBeganConn disconnect];
-        _touchBeganConn = nil;
-        [self cancelDrag];
-    }
+- (void)startDragWithTouch:(SPTouch*)touch {
+    [self startDragWithScreenLoc:[SPPoint pointWithX:touch.globalX y:touch.globalY]];
+}
+
+- (void)startDragWithScreenLoc:(SPPoint*)globalLoc {
+    _draggerReg = [self.mode.input registerListener:self];
+    _start = [globalLoc copy];
+    [self onDragStart:_start];
 }
 
 - (BOOL)onTouchStart:(SPPoint*)globalPt {
@@ -103,29 +61,5 @@
 - (void)onDragStart:(SPPoint*)start {}
 - (void)onDragged:(SPPoint*)current start:(SPPoint*)start {}
 - (void)onDragEnd:(SPPoint*)current start:(SPPoint*)start {}
-
-@end
-
-@implementation BTCallbackDragger
-
-@synthesize onDragStart, onDragged, onDragEnd;
-
-- (void)onDragStart:(SPPoint*)start {
-    if (onDragStart != nil) {
-        onDragStart(start);
-    }
-}
-
-- (void)onDragged:(SPPoint*)current start:(SPPoint*)start {
-    if (onDragged != nil) {
-        onDragged(current, start);
-    }
-}
-
-- (void)onDragEnd:(SPPoint*)current start:(SPPoint*)start {
-    if (onDragEnd != nil) {
-        onDragEnd(current, start);
-    }
-}
 
 @end
