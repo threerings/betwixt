4b04295635420665ca3c94178110864b2c15e317
Tim Conkling <tconkling@gmail.com>
2012-04-16 14:17:48 -0700

    add a very temporary Nod-specific DeviceType
    DeviceType is going to be ripped out soon and replaced with a more
    flexible scaling system.

--- Classes/BTDeviceType.h
+++ Classes/BTDeviceType.h
@@ -9,6 +9,7 @@
 + (BTDeviceType*)IPHONE_RETINA;
 + (BTDeviceType*)IPAD;
 + (BTDeviceType*)IPAD_RETINA;
++ (BTDeviceType*)NOD_TEMP;
 
 @property(nonatomic,readonly) int screenWidth;
 @property(nonatomic,readonly) int screenHeight;
--- Classes/BTDeviceType.m
+++ Classes/BTDeviceType.m
@@ -16,6 +16,7 @@ BT_ENUM_INIT(IPHONE, [[BTDeviceType alloc] initWithScreenWidth:480 screenHeight:
 BT_ENUM_INIT(IPHONE_RETINA, [[BTDeviceType alloc] initWithScreenWidth:960 screenHeight:640]);
 BT_ENUM_INIT(IPAD, [[BTDeviceType alloc] initWithScreenWidth:1024 screenHeight:768]);
 BT_ENUM_INIT(IPAD_RETINA, [[BTDeviceType alloc] initWithScreenWidth:2048 screenHeight:1536]);
+BT_ENUM_INIT(NOD_TEMP, [[BTDeviceType alloc] initWithScreenWidth:960.0f*2.67f screenHeight:640.0f*2.67f]);
 
 - (id)initWithScreenWidth:(int)screenWidth screenHeight:(int)screenHeight {
     if (!(self = [super init])) {
