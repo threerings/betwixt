5d86aec7a1b2d2a1fc1c7eeed13b62d837bcbd84
Tim Conkling <tconkling@gmail.com>
2012-04-17 13:23:33 -0700

    add SPTouch.touchId
    Each SPTouch has a unique ID that remains consistent through all touch phases,
    which makes it much easier to keep track of multiple simultaneous touches

--- Classes/BTInput+Package.h
+++
@@ -1,9 +0,0 @@
-//
-// Betwixt - Copyright 2012 Three Rings Design
-
-#import "BTInput.h"
-
-@interface BTInput (package)
-- (id)initWithMode:(BTMode*)mode;
-- (void)processTouches:(NSSet*)touches;
-@end
