a77d1cad1d4052c05aa328a756a9aee880921216
Tim Conkling <tconkling@gmail.com>
2012-07-22 18:36:30 -0700

    SPDisplayObjectContainer.spriteAtIndex/name

--- Classes/SPDisplayObjectContainer+BTExtensions.h
+++ Classes/SPDisplayObjectContainer+BTExtensions.h
@@ -3,6 +3,16 @@
 
 @interface SPDisplayObjectContainer (BTExtensions)
 
+/// Returns the SPSprite at the given index, or nil if no sprite is at that index
+- (SPSprite*)spriteAtIndex:(int)index;
+
+/// Returns the SPSprite with the given name, or nil if no sprite has that name
+- (SPSprite*)spriteByName:(NSString*)name;
+
+/// Returns the SPSprite with the given name, or nil if no sprite has that name
+- (SPSprite*)spriteByFormatName:(NSString*)format, ... NS_FORMAT_FUNCTION(1, 2);
+
+/// Returns the SPDisplayObject with the given name
 - (SPDisplayObject*)childByFormatName:(NSString*)format, ... NS_FORMAT_FUNCTION(1, 2);
 
 @end
--- Classes/SPDisplayObjectContainer+BTExtensions.m
+++ Classes/SPDisplayObjectContainer+BTExtensions.m
@@ -5,6 +5,20 @@
 
 @implementation SPDisplayObjectContainer (BTExtensions)
 
+- (SPSprite*)spriteAtIndex:(int)index {
+    SPDisplayObject* child = [self childAtIndex:index];
+    return ([child isKindOfClass:[SPSprite class]] ? (SPSprite*)child : nil);
+}
+
+- (SPSprite*)spriteByName:(NSString*)name {
+    SPDisplayObject* child = [self childByName:name];
+    return ([child isKindOfClass:[SPSprite class]] ? (SPSprite*)child : nil);
+}
+
+- (SPSprite*)spriteByFormatName:(NSString*)format, ... {
+    return [self spriteByName:OOO_FORMAT_TO_STRING(format)];
+}
+
 - (SPDisplayObject*)childByFormatName:(NSString*)format, ... {
     return [self childByName:OOO_FORMAT_TO_STRING(format)];
 }
