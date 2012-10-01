21b89679bf2479aef81b4035113f7807d76431ff
Tim Conkling <tconkling@gmail.com>
2012-10-01 12:50:14 -0700

    Add rect-creation functions to BTDisplayUtil

--- Classes/BTDisplayUtil.h
+++ Classes/BTDisplayUtil.h
@@ -5,3 +5,13 @@
 
 /// Blends two colors. 'amount' specifies the percentage of color1 that will be use
 uint32_t BTBlendARGB (uint32_t color1, uint32_t color2, float amount);
+
+@interface BTDisplayUtil : NSObject
+
++ (SPImage*)fillCircleWithRadius:(float)radius color:(uint)color;
++ (SPQuad*)fillRectWithWidth:(float)width height:(float)height color:(uint)color;
++ (SPSprite*)lineRectWithWidth:(float)width height:(float)height color:(uint)color
+                   outlineSize:(float)outlineSize;
++ (SPSprite*)fillRectWithWidth:(float)width height:(float)height color:(uint)color
+                   outlineSize:(float)outlineSize outlineColor:(uint)outlineColor;
+@end
--- Classes/BTDisplayUtil.m
+++ Classes/BTDisplayUtil.m
@@ -2,6 +2,7 @@
 // Betwixt - Copyright 2012 Three Rings Design
 
 #import "BTDisplayUtil.h"
+#import "UIColor+BTExtensions.h"
 
 uint32_t BTBlendARGB (uint32_t color1, uint32_t color2, float amount) {
     if (amount <= 0) {
@@ -17,3 +18,57 @@ uint32_t BTBlendARGB (uint32_t color1, uint32_t color2, float amount) {
     float b = (amount * (float) SP_COLOR_PART_BLUE(color1)) + (inv * (float) SP_COLOR_PART_BLUE(color2));
     return BT_COMPOSE_ARGB(a, r, g, b);
 }
+
+@implementation BTDisplayUtil
+
++ (SPImage*)fillCircleWithRadius:(float)radius color:(uint)color {
+    int size = (int) ceil(radius * 2);
+
+    SPTexture* tex = [[SPTexture alloc] initWithWidth:size height:size draw:^(CGContextRef ctx) {
+        CGContextSetFillColorWithColor(ctx, [UIColor colorWithARGB:color | 0xff000000].CGColor);
+        CGContextFillEllipseInRect(ctx, CGRectMake(0, 0, size, size));
+    }];
+
+    SPImage* img = [[SPImage alloc] initWithTexture:tex];
+    img.pivotX = radius;
+    img.pivotY = radius;
+    return img;
+}
+
++ (SPQuad*)fillRectWithWidth:(float)width height:(float)height color:(uint)color {
+    SPQuad* quad = [[SPQuad alloc] initWithWidth:width height:height];
+    quad.color = color;
+    return quad;
+}
+
++ (SPSprite*)lineRectWithWidth:(float)width height:(float)height color:(uint)color
+                   outlineSize:(float)outlineSize {
+
+    return [BTDisplayUtil fillRectWithWidth:width height:height color:0 outlineSize:outlineSize
+                             outlineColor:color];
+}
+
++ (SPSprite*)fillRectWithWidth:(float)width height:(float)height color:(uint)color
+                   outlineSize:(float)outlineSize outlineColor:(uint)outlineColor {
+
+    SPSprite* sprite = [SPSprite sprite];
+
+    if (color != 0) {
+        [sprite addChild:[BTDisplayUtil fillRectWithWidth:width height:height color:color]];
+    }
+    SPQuad* top = [BTDisplayUtil fillRectWithWidth:width height:outlineSize color:outlineColor];
+    SPQuad* bottom = [BTDisplayUtil fillRectWithWidth:width height:outlineSize color:outlineColor];
+    SPQuad* left = [BTDisplayUtil fillRectWithWidth:outlineSize height:height color:outlineColor];
+    SPQuad* right = [BTDisplayUtil fillRectWithWidth:outlineSize height:height color:outlineColor];
+
+    bottom.y = height - outlineSize;
+    right.x = width - outlineSize;
+
+    [sprite addChild:top];
+    [sprite addChild:bottom];
+    [sprite addChild:left];
+    [sprite addChild:right];
+    return sprite;
+}
+
+@end
